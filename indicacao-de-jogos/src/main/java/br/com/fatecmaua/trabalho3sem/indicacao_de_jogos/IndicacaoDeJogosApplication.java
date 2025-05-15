cdb49c6bdca307d376908c1ba917abe8b3337c8d
HttpsFelps <luisf.foresto@gmail.com>
2025-05-15 16:35:43 -0300

    Criação do projeto

---
+++ indicacao-de-jogos/src/main/java/br/com/fatecmaua/trabalho3sem/indicacao_de_jogos/IndicacaoDeJogosApplication.java
@@ -0,0 +1,13 @@
+package br.com.fatecmaua.trabalho3sem.indicacao_de_jogos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndicacaoDeJogosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndicacaoDeJogosApplication.class, args);
+	}
+
+}
---
+++ indicacao-de-jogos/src/test/java/br/com/fatecmaua/trabalho3sem/indicacao_de_jogos/IndicacaoDeJogosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fatecmaua.trabalho3sem.indicacao_de_jogos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndicacaoDeJogosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
