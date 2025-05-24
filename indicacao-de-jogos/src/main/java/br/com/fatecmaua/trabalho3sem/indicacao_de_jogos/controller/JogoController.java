8dd7375ff1799b93997371f144d29b4d0e6ca85f
HttpsFelps <luisf.foresto@gmail.com>
2025-05-24 15:30:59 -0300

    Correção da falta do argumento post na controller

--- indicacao-de-jogos/src/main/java/br/com/fatecmaua/trabalho3sem/indicacao_de_jogos/controller/JogoController.java
+++ indicacao-de-jogos/src/main/java/br/com/fatecmaua/trabalho3sem/indicacao_de_jogos/controller/JogoController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -64,6 +65,7 @@ public class JogoController {
 		}
 	}
 	
+	@PutMapping(value = "/atualiza/{id}")
 	public Jogo atualizaJogo(@PathVariable Long id, @RequestBody Jogo jogo_atualizado) {
 		Optional<Jogo> op = repJ.findById(id);
 		
